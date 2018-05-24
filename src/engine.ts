262448f50a78c13235d4e0a75c93f727015266a2
Cody Stewart <cjstewart88@gmail.com>
2018-05-24 00:55:35 -0400

    first shot at navigating to a location thats clicked

--- src/engine.ts
+++ src/engine.ts
@@ -14,7 +14,7 @@ class Engine {
     this.setCanvasSize()
 
     window.addEventListener('resize', this.setCanvasSize.bind(this), false);
-    window.requestAnimationFrame(this.draw.bind(this));
+    this.draw();
   }
 
   private setCanvasSize() {
@@ -25,9 +25,56 @@ class Engine {
 
   private draw() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.checkMove();
     this.ctx.fillStyle = 'rgb(0, 0, 0)';
-    this.ctx.fillRect(this.player.x, this.player.y, 10, 10);
+    this.ctx.fillRect(this.player.currentPosition.x, this.player.currentPosition.y, 10, 10);
     window.requestAnimationFrame(this.draw.bind(this));
+  }
+
+  private checkedCordinates: any = {};
+  private checkMove() {
+    if (this.player.moveInProgress === false) {
+      return;
+    }
+    
+    this.checkedCordinates = {};
+
+    for (let i = 1; i <= 8; i++) {
+      if (i == 1) {
+        this.checkedCordinates[i] = { x: this.player.currentPosition.x - 10, y: this.player.currentPosition.y - 10 }
+      } else if (i == 2) {
+        this.checkedCordinates[i] = { x: this.player.currentPosition.x, y: this.player.currentPosition.y - 10 }
+      } else if (i == 3) {
+        this.checkedCordinates[i] = { x: this.player.currentPosition.x + 10, y: this.player.currentPosition.y - 10 }
+      } else if (i == 4) {
+        this.checkedCordinates[i] = { x: this.player.currentPosition.x + 10, y: this.player.currentPosition.y }
+      } else if (i == 5) {
+        this.checkedCordinates[i] = { x: this.player.currentPosition.x + 10, y: this.player.currentPosition.y + 10 }
+      } else if (i == 6) {
+        this.checkedCordinates[i] = { x: this.player.currentPosition.x, y: this.player.currentPosition.y + 10 }
+      } else if (i == 7) {
+        this.checkedCordinates[i] = { x: this.player.currentPosition.x - 10, y: this.player.currentPosition.y + 10 }
+      } else if (i == 8) {
+        this.checkedCordinates[i] = { x: this.player.currentPosition.x - 10, y: this.player.currentPosition.y }
+      }
+    }
+
+    let closestMove = '1';
+    for (const key in this.checkedCordinates) {
+      const cordinate = this.checkedCordinates[key];
+      this.checkedCordinates[key].xDiff = Math.abs(cordinate.x - this.player.requestedPosition.x); 
+      this.checkedCordinates[key].yDiff = Math.abs(cordinate.y - this.player.requestedPosition.y); 
+      if (this.checkedCordinates[key].xDiff < this.checkedCordinates[closestMove].xDiff && this.checkedCordinates[key].yDiff < this.checkedCordinates[closestMove].yDiff) {
+        closestMove = key;
+      }
+    }
+
+    this.player.currentPosition.x = this.checkedCordinates[closestMove].x;
+    this.player.currentPosition.y = this.checkedCordinates[closestMove].y;
+  
+    if (this.player.currentPosition.x === this.player.requestedPosition.x && this.player.currentPosition.y === this.player.requestedPosition.y) {
+      this.player.moveInProgress = false;
+    }
   }
 }
 
--- src/player.ts
+++ src/player.ts
@@ -1,14 +1,16 @@
 export class Player {
-  public x: number = 0;
-  public y: number = 0;
+  public currentPosition = { x: 0, y: 0 };
+  public requestedPosition = { x: 0, y: 0};
+  public moveInProgress: boolean = false;
   public gold: number = 0;
 
   constructor() {
-    window.addEventListener('click', this.movePlayer.bind(this));
+    window.addEventListener('click', this.requestMove.bind(this));
   }
 
-  private movePlayer(event: MouseEvent) {
-    this.x = event.clientX;
-    this.y = event.clientY;
+  private requestMove(event: MouseEvent) {
+    this.requestedPosition.x = Math.round(event.clientX / 10) * 10;
+    this.requestedPosition.y = Math.round(event.clientY / 10) * 10;
+    this.moveInProgress = true;
   }
 }
