cc470930c06e2c543c1c0d132ea8d7029b0dfe97
Cody Stewart <cjstewart88@gmail.com>
2018-05-24 23:57:33 -0400

    refactor player movement and fix movement bug

--- src/player.ts
+++ src/player.ts
@@ -1,16 +1,55 @@
 export class Player {
   public currentPosition = { x: 0, y: 0 };
-  public requestedPosition = { x: 0, y: 0};
-  public moveInProgress: boolean = false;
+  public requestedPosition: { x: number, y: number } | undefined;
+  //public moveInProgress: boolean = false;
   public gold: number = 0;
 
   constructor() {
     window.addEventListener('click', this.requestMove.bind(this));
   }
 
+  public get moving(): boolean {
+    return this.requestedPosition !== undefined;
+  }
+  
+  public get idle(): boolean {
+    return this.requestedPosition === undefined;
+  }
+
+  public move() {
+    if (this.idle) return;
+    
+    let possibleMoves: any[] = [
+      { id: 1, x: this.currentPosition.x - 10, y: this.currentPosition.y - 10 },
+      { id: 2, x: this.currentPosition.x, y: this.currentPosition.y - 10 },
+      { id: 3, x: this.currentPosition.x + 10, y: this.currentPosition.y - 10 },
+      { id: 4, x: this.currentPosition.x + 10, y: this.currentPosition.y },
+      { id: 5, x: this.currentPosition.x + 10, y: this.currentPosition.y + 10 },
+      { id: 6, x: this.currentPosition.x, y: this.currentPosition.y + 10 },
+      { id: 7, x: this.currentPosition.x - 10, y: this.currentPosition.y + 10 },
+      { id: 8, x: this.currentPosition.x - 10, y: this.currentPosition.y }
+    ];
+
+    let closestMove = possibleMoves[0];
+    possibleMoves.forEach(possibleMove => {
+      possibleMove.distanceFromRequestedMove = Math.abs(possibleMove.x - this.requestedPosition.x) + Math.abs(possibleMove.y - this.requestedPosition.y);
+      if (possibleMove.distanceFromRequestedMove < closestMove.distanceFromRequestedMove) {
+        closestMove = possibleMove;
+      }
+    });
+    
+    this.currentPosition.x = closestMove.x;
+    this.currentPosition.y = closestMove.y;
+
+    if (this.currentPosition.x === this.requestedPosition.x && this.currentPosition.y === this.requestedPosition.y) {
+      this.requestedPosition = undefined;
+    }
+  }
+
   private requestMove(event: MouseEvent) {
-    this.requestedPosition.x = Math.round(event.clientX / 10) * 10;
-    this.requestedPosition.y = Math.round(event.clientY / 10) * 10;
-    this.moveInProgress = true;
+    this.requestedPosition = {
+      x: Math.round(event.clientX / 10) * 10,
+      y:  Math.round(event.clientY / 10) * 10
+    }
   }
 }
