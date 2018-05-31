af87cdacb8e43082683c8825da0a7b4c46ae7555
Cody Stewart <cjstewart88@gmail.com>
2018-05-31 00:35:40 -0400

    use PathFinding.js's Dijkstra algorithm

--- src/engine.ts
+++ src/engine.ts
@@ -1,15 +1,21 @@
 import { Player} from './player';
 import { Game } from './game';
+import * as PF from 'pathfinding';
+
 
 export class Engine {
   private player: Player;
   private level: any;
+  private zeroOneMatrixOfLevel: any;
+
+  private pathToRequestedPosition: any;
 
   private canvas: any;
   private ctx: any;
 
   constructor(game: Game) {
     this.level = game.level;
+    this.zeroOneMatrixOfLevel = this.toZeroOneMatrix();
     this.player = game.player;
 
     this.canvas = document.getElementById('level');
@@ -17,20 +23,60 @@ export class Engine {
     this.canvas.width = 800;
     this.canvas.height = 600;
 
-    this.draw();
+    this.animate();
   }
 
-  private draw() {
-    this.ctx.clearRect(0, 0, 800, 600);
-    this.player.move();
+  private animate() {
+    this.movePlayerIfRequested();
     this.drawLevel();
-    window.requestAnimationFrame(this.draw.bind(this));
+    window.requestAnimationFrame(this.animate.bind(this));
+  }
+
+  private toZeroOneMatrix() {
+    let nRow = this.level.length;
+    let nCol = this.level[0].length;
+    let matrix = new Array(nRow);
+
+    for (let y = 0; y < nRow; y++) {
+      matrix[y] = new Array(nCol);
+      for (let x = 0; x < nCol; x++) {
+        matrix[y][x] = this.level[y][x] === 'W' ?  1 : 0;
+      }
+    }
+
+    return matrix;
+  }
+
+  private movePlayerIfRequested() {
+    if (!this.player.requestedPosition) {
+      return;
+    }
+
+    if (this.pathToRequestedPosition === undefined) {
+      const pathFinder = new PF.DijkstraFinder();
+      const grid = new PF.Grid(this.zeroOneMatrixOfLevel);
+      this.pathToRequestedPosition = pathFinder.findPath(this.player.currentPosition.x, this.player.currentPosition.y, this.player.requestedPosition.x, this.player.requestedPosition.y, grid);
+    } 
+
+    if (this.pathToRequestedPosition.length === 0) {
+      this.player.requestedPosition = undefined;
+      this.pathToRequestedPosition = undefined;
+      return;
+    }
+
+    const nextPlayerPosition = this.pathToRequestedPosition.shift(); 
+    this.level[this.player.currentPosition.y][this.player.currentPosition.x] = '0';
+    this.player.currentPosition.x = nextPlayerPosition[0];
+    this.player.currentPosition.y = nextPlayerPosition[1];
+    this.level[this.player.currentPosition.y][this.player.currentPosition.x] = 'P';
   }
 
   private drawLevel() {
+    this.ctx.clearRect(0, 0, 800, 600);
+    
     this.level.forEach((row: any[], i: number) => {
-      row.forEach((cellValue: string, ii: number) => {
-        if (cellValue === 'E') {
+      row.forEach((cellValue: number | string, ii: number) => {
+        if (cellValue === '0') {
           return;
         }
 
--- src/player.ts
+++ src/player.ts
@@ -1,82 +1,15 @@
 import { Game } from './game';
 
 export class Player {
-  private currentPosition = { x: 0, y: 0 };
-  private requestedPosition: { x: number, y: number } | undefined;
-  private alreadyVisited: any[] = [];
-  private level: any;
+  public currentPosition = { x: 0, y: 0 };
+  public requestedPosition: { x: number, y: number } | undefined;
   
   constructor(game: Game) {
-    this.level = game.level;
-
     document.getElementById('level')
-      .addEventListener('click', this.requestMove.bind(this));
+      .addEventListener('click', this.move.bind(this));
   }
 
-  public get moving(): boolean {
-    return this.requestedPosition !== undefined;
-  }
-
-  public get idle(): boolean {
-    return this.requestedPosition === undefined;
-  }
-
-  public move() {
-    if (this.idle) return;
-
-    let possibleMoves: any[] = [
-      { id: 1, x: this.currentPosition.x - 1, y: this.currentPosition.y - 1 },
-      { id: 2, x: this.currentPosition.x, y: this.currentPosition.y - 1 },
-      { id: 3, x: this.currentPosition.x + 1, y: this.currentPosition.y - 1 },
-      { id: 4, x: this.currentPosition.x + 1, y: this.currentPosition.y },
-      { id: 5, x: this.currentPosition.x + 1, y: this.currentPosition.y + 1 },
-      { id: 6, x: this.currentPosition.x, y: this.currentPosition.y + 1 },
-      { id: 7, x: this.currentPosition.x - 1, y: this.currentPosition.y + 1 },
-      { id: 8, x: this.currentPosition.x - 1, y: this.currentPosition.y }
-    ];
-
-    let impossibleMoves: number[] = [];
-    possibleMoves.forEach((pm, i, array) => {
-      if (pm.x === -1 || pm.y === -1) {
-        impossibleMoves.push(pm.id);
-      } else if (this.level[pm.y][pm.x] === 'W') {
-        impossibleMoves.push(pm.id);
-      } else if (this.alreadyVisited.indexOf(`${pm.x},${pm.y}`) !== -1) {
-        impossibleMoves.push(pm.id);
-      }
-    });
-    
-    let closestMove: any;
-    possibleMoves.forEach(possibleMove => {
-      if (impossibleMoves.indexOf(possibleMove.id) !== -1) {
-        return;
-      }
-
-      if (!closestMove) {
-        closestMove = possibleMove;
-      }
-
-      possibleMove.distanceFromRequestedMove = Math.abs(possibleMove.x - this.requestedPosition.x) + Math.abs(possibleMove.y - this.requestedPosition.y);
-
-      if (possibleMove.distanceFromRequestedMove < closestMove.distanceFromRequestedMove) {
-        closestMove = possibleMove;
-      }
-    });
-
-    this.alreadyVisited.push(`${closestMove.x},${closestMove.y}`);
-
-    this.level[this.currentPosition.y][this.currentPosition.x] = 'E';  
-    this.currentPosition.x = closestMove.x;
-    this.currentPosition.y = closestMove.y;
-    this.level[this.currentPosition.y][this.currentPosition.x] = 'P';
-
-    if (this.currentPosition.x === this.requestedPosition.x && this.currentPosition.y === this.requestedPosition.y) {
-      this.requestedPosition = undefined;
-      this.alreadyVisited = [];
-    }
-  }
-
-  private requestMove(event: MouseEvent) {
+  private move(event: MouseEvent) {
     this.requestedPosition = {
       x: Math.floor(event.offsetX/10),
       y: Math.floor(event.offsetY/10)
